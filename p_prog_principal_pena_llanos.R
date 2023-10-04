d8d87d28457b1275d7a3bef1db224f05eda87b86
Angie Ll <aellanosc@unal.edu.co>
2023-10-04 12:47:02 -0500

    THIS IS THE FIRST STEP FOR SUBMIT CHANGES!

---
+++ p_prog_principal_pena_llanos.R
@@ -0,0 +1,23 @@
+
+# Modelo de estimación y credibilidad
+# CREDIBILIDAD Y MODELOS DE PÉRDIDA
+# UNIVERSIDAD NACIONAL DE COLOMBIA
+# Maestría en Actuaría y Finanzas
+# 2023-II
+
+
+# Presentado por: Camila Peña, Angie Llanos
+# Presentado a: Edwin Riveros
+
+
+## Tarea 1
+## This is just for a proof
+
+## Tarea 2
+
+
+## Tarea 3
+
+
+## Tarea 4
+
